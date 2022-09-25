3ca4ced4f5cfb982907a7892ecb85fe2df147997
Deniz Işık <denizisik2004@gmail.com>
2022-09-25 17:22:55 +0300

    Script written, working as intended

--- JS-Fundamentals/script.js
+++ JS-Fundamentals/script.js
@@ -1,4 +1,9 @@
 let playerChoice = "";
+let winCheck = {
+    'rock' : 'scissors',
+    'paper': 'rock',
+    'scissors' : 'paper'
+};
 
 let choiceArray = [
     'rock',
@@ -6,17 +11,43 @@ let choiceArray = [
     'scissors'
 ]
 
+let playerScore = 0;
+let compScore = 0;
+
 let roundOver = false;
+const resultDiv = document.getElementById('result');
+
 
 function roundStart(choice) {
     if(!roundOver) {
         playerChoice = String(choice);
-        console.log(`playerChoice updated with ${playerChoice}`)
         compChoice = choiceArray[Math.floor(Math.random()*choiceArray.length)];
-        console.log(`compChoice updated with ${compChoice}`)
+        console.log(`playerChoice: ${playerChoice}\ncompChoice: ${compChoice}`);
 
         if(playerChoice == compChoice) {
-            
+            resultDiv.textContent = "It's a tie!";
         }
+        else if (winCheck[playerChoice] == compChoice){
+            playerScore++;
+            resultDiv.textContent = `${playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)} beats ${compChoice}!!`;
+            updateScore();
+        }
+        else {
+            compScore++;
+            resultDiv.textContent = `${compChoice.charAt(0).toUpperCase() + compChoice.slice(1)} beats ${playerChoice}!!`;
+            updateScore();
+        }
+    }
+    else {
+        alert("Round is over! Refresh the page to play again.")
+    }
+}
+
+function updateScore() {
+    (document.getElementById('pscore')).textContent = playerScore;
+    (document.getElementById('cscore')).textContent = compScore;
+    if (playerScore == 5 || compScore == 5) {
+        roundOver = true;
+        playerScore > compScore ? resultDiv.textContent = "PLAYER WINS!" : resultDiv.textContent = "COMPUTER WINS!" 
     }
 }
