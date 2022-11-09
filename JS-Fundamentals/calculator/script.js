5504bac1955ecd7cfeb8cd06477eac959c76a412
Deniz Işık <denizisik2004@gmail.com>
2022-11-09 18:01:40 +0300

    Implemented operation displaying

--- JS-Fundamentals/calculator/script.js
+++ JS-Fundamentals/calculator/script.js
@@ -2,14 +2,23 @@ let ops = document.getElementById('ops');
 let current = document.getElementById('current');
 
 var nodes = document.getElementById('grid').getElementsByTagName('button');
-for(var i=0; i<nodes.length; i++) {
-    nodes[i].addEventListener("click", function(event){updateCurrent(event)});
+for(let i=0; i<nodes.length; i++) {
+    if((nodes[i].className).includes('number')){
+        nodes[i].addEventListener("click", function(event){updateCurrent(event)});   
+    } else if((nodes[i].className).includes('operator')) {
+        nodes[i].addEventListener("click", function(event){operationUpdate(event)});   
+    }
+}
+
+var utilities = document.getElementById('utilities').getElementsByTagName('button');
+for(let i=0; i<utilities.length; i++) {
+    utilities[i].addEventListener("click", function(event){reconfigureCalc(event)});
 }
 
 function updateCurrent(event) {
     node = event.target;
     nodeClass = (node.className).replace('blue', '').replace('orange', '').replace(/\s/g, '')
-    if(nodeClass == 'number') {
+    if(ops.innerHTML == '') {
         if(current.innerHTML == '0') {
             current.innerHTML = node.innerHTML;
         }
@@ -19,18 +28,18 @@ function updateCurrent(event) {
     }
 }
 
-var utilities = document.getElementById('utilities').getElementsByTagName('button');
-for(var i=0; i<utilities.length; i++) {
-    utilities[i].addEventListener("click", function(event){reconfigureCalc(event)});
-}
-
 function reconfigureCalc(event) {
     isClear = false;
     event.target.id == 'clear' ? isClear = true : isClear = false;
     if(isClear){
         current.innerHTML = '0';
+        ops.innerHTML = '';
     } else {
-        console.log(current.innerHTML)
         current.innerHTML = (current.innerHTML).slice(0, -1);
     }
+}
+
+function operationUpdate(event) {
+    let op = event.target;
+    ops.innerHTML = current.innerHTML + ' ' + op.innerHTML + ' ';
 }
