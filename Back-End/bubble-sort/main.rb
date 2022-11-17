a8043d40d3d2d6af954d9210785e9dc6858de220
Deniz Işık <denizisik2004@gmail.com>
2022-11-17 10:19:47 +0300

    TODO comment removed

--- Back-End/bubble-sort/main.rb
+++ Back-End/bubble-sort/main.rb
@@ -5,8 +5,6 @@ def bubble_sort(array = [])
     return nil if array.empty?
     unsorted = true
 
-    # Implement recursion
-
     while unsorted do
         current = array.map(&:clone)
         for i in 0..(array.length-2) do
