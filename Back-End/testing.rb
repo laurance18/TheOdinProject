f376b3679817bfab437f0fed5e7a09c73193d71b
Deniz Işık <denizisik2004@gmail.com>
2022-11-17 10:19:56 +0300

    Messing around with class properties

--- Back-End/testing.rb
+++ Back-End/testing.rb
@@ -1,8 +1,39 @@
 require 'pry-byebug'
 
-array = [1,2,3]
-current = [1,2,3]
+# @ instance var - createda after init
+# @@ class (static) var - created when program runs
+# $ global var - cretaed when program runs, can be accessed without an instance
 
-p current == array
+class Computer
+    $manufacturer = "Mango Computer, Inc."
+    @@files = {hello: "Hello, world!"}
+    
+    def initialize(username, password)
+      @username = username
+      @password = password
+    end
+    
+    def current_user
+      @username
+    end
+    
+    def self.display_files
+      @@files
+    end
+  end
+  
+  # Make a new Computer instance:
+  hal = Computer.new("Dave", 12345)
+  
+  puts "Current user: #{hal.current_user}"
+  # @username belongs to the hal instance.
+  
+  puts "Manufacturer: #{$manufacturer}"
+  # $manufacturer is global! We can get it directly.
+  
+  puts "Files: #{Computer.display_files}"
+  # @@files belongs to the Computer class.
 
-
+  i = 0
+  ++i
+  puts i
