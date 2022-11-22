c484bb121199219b932172ce588eeb73955e55b7
Deniz Işık <denizisik2004@gmail.com>
2022-11-22 21:24:52 +0300

    HEREDOC testing

--- Back-End/testing.rb
+++ Back-End/testing.rb
@@ -1,35 +1,12 @@
 require 'pry-byebug'
 
-# @ instance var - createda after init
-# @@ class (static) var - created when program runs
-# $ global var - cretaed when program runs, can be accessed without an instance
 
-class Computer
-  $manufacturer = "Mango Computer, Inc."
-  @@files = {hello: "Hello, world!"}
-    
-  def initialize(username, password)
-    @username = username
-    @password = password
-  end
-    
-  def current_user
-    @username
-  end
-    
-  def self.display_files
-    @@files
-  end
-end
-  
-# Make a new Computer instance:
-hal = Computer.new("Dave", 12345)
-  
-puts "Current user: #{hal.current_user}"
-# @username belongs to the hal instance.
-  
-puts "Manufacturer: #{$manufacturer}"
-# $manufacturer is global! We can get it directly.
-  
-puts "Files: #{Computer.display_files}"
-# @@files belongs to the Computer class.
+a =  <<-QUA
+       test | test | test
+       -----+------+-----
+       test | test | test
+       -----+------+-----
+       test | test | test
+    QUA
+
+puts a
