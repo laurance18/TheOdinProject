c6a3352371a9828a24f9d2ff4e854765fe2258f9
Deniz Işık <denizisik2004@gmail.com>
2022-11-22 21:25:00 +0300

    init player class

---
+++ Back-End/tic-tac-toe/player.rb
@@ -0,0 +1,12 @@
+
+# Player class for the two players
+
+class Player
+    attr_reader :name
+    attr_reader :symbol
+
+    def initialize(name, symbol)
+        @name = name
+        @symbol = symbol
+
+end
