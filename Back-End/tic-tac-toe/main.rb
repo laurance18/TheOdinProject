867e4640fc3e4c4a02dd0c6fc7c1b3e76378a193
Deniz Işık <denizisik2004@gmail.com>
2022-11-24 13:42:59 +0300

    Update main.rb

--- Back-End/tic-tac-toe/main.rb
+++ Back-End/tic-tac-toe/main.rb
@@ -2,3 +2,18 @@ require 'pry-byebug'
 require_relative 'grid.rb'
 require_relative 'player.rb'
 
+print "Name of the player #1: "
+curr_name = gets.chomp
+print "Symbol for the player #1: "
+curr_symbol = gets.chomp
+
+playerOne = Player.new(curr_name, curr_symbol)
+
+print "\nName of the player #2: "
+curr_name = gets.chomp
+print "Symbol for the player #2: "
+curr_symbol = gets.chomp
+
+playerTwo = Player.new(curr_name, curr_symbol)
+
+print playerTwo.name
