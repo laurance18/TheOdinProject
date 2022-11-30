138b43a758d4987e78ef2a7df717cce455936101
Deniz Işık <denizisik2004@gmail.com>
2022-11-30 23:42:22 +0300

    Class implementation completed

--- Back-End/tic-tac-toe/game.rb
+++ Back-End/tic-tac-toe/game.rb
@@ -3,23 +3,22 @@ require_relative 'grid.rb' # load GridVariables
 # Game mechanics
 
 def play_game(playerOne, playerTwo)
-  board = Marshal.load(Marshal.dump(GridVariables::BOARD)) 
-  board_indexes = Marshal.load(Marshal.dump(GridVariables::BOARD_ARR)) 
-  puts "\n#{board}"
+  grid = Grid.new()
+  puts "\n#{grid.board}"
 
   game_ongoing = true
   player = playerOne # Inital player
   while game_ongoing
-    turn(player, board, board_indexes)
+    turn(player, grid)
   end
 end
 
-def turn(player, board, board_indexes)
+def turn(player, grid)
   print "#{player.name}, choose a spot (1-9) available to occupy with #{player.symbol}: "
   choice = gets.chomp.to_i
-  if choice.between?(1,9) && board_indexes[choice - 1]
-    board_indexes[choice - 1] = player.symbol
-    print "Placement succesfull!"
-    print board
+  if choice.between?(1,9) && (1..9).to_a.include?(grid.indexes[choice - 1])
+    grid.updateBoard(choice-1, player.symbol)
+    puts "Placement succesfull!"
+    puts "\n#{grid.board}"
   end
 end
--- Back-End/tic-tac-toe/grid.rb
+++ Back-End/tic-tac-toe/grid.rb
@@ -1,19 +1,34 @@
 
-# Grid Variables
-module GridVariables
-  # Board setup
+# Grid Class
 
-  BOARD_ARR = (1..9).to_a
-  BOARD =  <<-HEREDOC
-      #{BOARD_ARR[0]} | #{BOARD_ARR[1]} | #{BOARD_ARR[2]}
-    ----+---+----
-      #{BOARD_ARR[3]} | #{BOARD_ARR[4]} | #{BOARD_ARR[5]}
-    ----+---+----
-      #{BOARD_ARR[6]} | #{BOARD_ARR[7]} | #{BOARD_ARR[8]}
-  HEREDOC
+class Grid
+  attr_accessor :indexes
+  attr_accessor :board
+  attr_reader :COMBOS
 
-  WINNING_COMBOS = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
-    [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
-  ].freeze # Winning combinations
+  def initialize()
+    @indexes = (1..9).to_a
+    @board = <<-HEREDOC
+      1 | 2 | 3
+    ----+---+----
+      4 | 5 | 6
+    ----+---+----
+      7 | 8 | 9
+    HEREDOC
+    @COMBOS = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+        [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+      ].freeze # Winning combinations
+  end
+
+  def updateBoard(index, symbol)
+    @indexes[index] = symbol
+    @board = <<-HEREDOC
+      #{@indexes[0]} | #{@indexes[1]} | #{@indexes[2]}
+    ----+---+----
+      #{@indexes[3]} | #{@indexes[4]} | #{@indexes[5]}
+    ----+---+----
+      #{@indexes[6]} | #{@indexes[7]} | #{@indexes[8]}
+    HEREDOC
+  end
 end
