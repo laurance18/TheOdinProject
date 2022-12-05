4430acd5740c89b1d7eda9f653f104bef425100c
Deniz Işık <denizisik2004@gmail.com>
2022-12-06 00:32:06 +0300

    Functions defined

--- Back-End/ruby-projects/mastermind/game.rb
+++ Back-End/ruby-projects/mastermind/game.rb
@@ -1,3 +1,22 @@
 # Game class
 
 class Game
+  def initialize()
+    @colors = ['blue', 'green', 'red', 'yellow', 'orange', 'purple', 'pink']
+    @picked_color = ""
+    @current_feedback = []
+  end
+
+  def play()
+    print("Welcome to the game Mastermind! A random color has been picked, you have 8 turns to guess the color. Feedback will be provided accordingly.")
+    @picked_color = colors[rand(0..colors.length-1)] # Computer picks random color
+  end
+
+  def turn()
+    
+  end
+
+  def feedback()
+
+  end
+end
--- Back-End/ruby-projects/mastermind/main.rb
+++ Back-End/ruby-projects/mastermind/main.rb
@@ -1,6 +1,2 @@
 require 'pry-byebug'
 
-colors = ['blue', 'green', 'red', 'yellow', 'orange', 'purple', 'pink']
-random_color = colors[rand(0..colors.length-1)] # Computer picks random color
-
-
