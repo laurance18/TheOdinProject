5c771b03595ddb9d613098a9e4f5dfa4bcfa9c3e
Deniz Işık <denizisik2004@gmail.com>
2022-12-12 22:42:11 +0300

    Update testing.rb

--- Back-End/testing.rb
+++ Back-End/testing.rb
@@ -10,3 +10,9 @@ a =  <<-QUA
     QUA
 
 puts a
+
+def indices(words, searched_word)
+    words.each_index.select { |index| words[index] == searched_word }
+  end
+
+print indices(["a", "b","a"], "a")
