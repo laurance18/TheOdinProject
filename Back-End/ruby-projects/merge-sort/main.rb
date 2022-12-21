222ad8d2c0345bdd93df8d3b5a15c68157828350
Deniz Işık <denizisik2004@gmail.com>
2022-12-21 17:42:09 +0300

    Update main.rb

--- Back-End/ruby-projects/merge-sort/main.rb
+++ Back-End/ruby-projects/merge-sort/main.rb
@@ -1,37 +1,31 @@
-
 def merge_sort(array)
-  if array.size < 2
-    array
-  else
-    left = merge_sort(array[0...array.size / 2])
-    right = merge_sort(array[array.size / 2...array.size])
-    merge(left, right)
-  end
+  return array if array.size <= 1
+
+  mid = array.size / 2
+  left = array[0, mid]
+  right = array[mid, array.size]
+
+  left = merge_sort(left)
+  right = merge_sort(right)
+
+  merge(left, right)
 end
 
-def merge(left, right, array = [])
-  (left.size + right.size).times do
-    if left.empty?
-      array << right.shift
-    elsif right.empty?
-      array << left.shift
+def merge(left, right)
+  sorted = []
+
+  until left.empty? || right.empty?
+    if left.first <= right.first
+      sorted << left.shift
     else
-      comparison = left <=> right
-      if comparison == -1
-        array << left.shift
-      elsif comparison == 1
-        array << right.shift
-      else
-        array << left.shift
-      end
+      sorted << right.shift
     end
   end
-  array
+
+  sorted.concat(left).concat(right)
 end
 
-arr = []
-rand(200).times do
-  arr << rand(200)
-end
-
-print merge_sort(arr)
+# Example usage
+array = [5, 3, 4, 1, 2]
+sorted_array = merge_sort(array)
+puts sorted_array 
