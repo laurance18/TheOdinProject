b392363ea91540a986de1bd462bbc7dc60dc730c
Deniz Işık <denizisik2004@gmail.com>
2022-12-22 15:10:34 +0300

    Append method added

--- Back-End/ruby-projects/linked-list/main.rb
+++ Back-End/ruby-projects/linked-list/main.rb
@@ -18,16 +18,25 @@ class LinkedList
 
   def prepend(value)
     new_node = Node.new(value)
-    unless @head == nil
+    unless @head == nil # Check if head is already present, if so, prepend accordingly
       new_node.link = @head
       @head = new_node
     else
-      @head = new_node
+      @head = new_node # If no head is present, head = new_node
     end
   end
 
   def append(value)
-    
+    new_node = Node.new(value)
+    unless @head == nil # Check if head is already present, if so, check until current.link == nil, then append
+      current = @head
+      until current.link == nil
+        current = current.link
+      end
+      current.link = new_node
+    else
+      @head = new_node # If no head is present, head = new_node
+    end
   end
 
 end
