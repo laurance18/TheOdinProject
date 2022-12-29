4d437f576621a7b5b700191b444dc565fb50ff1b
Deniz Işık <denizisik2004@gmail.com>
2022-12-29 16:44:39 +0300

    Update main.rb

--- Back-End/ruby-projects/binary-search-tree/main.rb
+++ Back-End/ruby-projects/binary-search-tree/main.rb
@@ -17,8 +17,22 @@ class Tree
   end
 
   def build_tree(array)
-    array.uniq!.sort!
-    
+    array.empty? ? nil : array = array.uniq.sort
+    current_line = 1
+    levels = 1 + Math.log(array.size, 2).floor
+
+    until current_line == levels
+      iter = Array.new()
+      for i in (0...2**(levels-1)) do
+        next
+      end
+    end
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 
 end
