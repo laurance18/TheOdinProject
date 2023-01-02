9602c0c1666f8d16367c0f6946f065b079333d32
Deniz Işık <denizisik2004@gmail.com>
2023-01-02 21:30:44 +0300

    #build_tree method added

--- Back-End/ruby-projects/binary-search-tree/main.rb
+++ Back-End/ruby-projects/binary-search-tree/main.rb
@@ -1,7 +1,7 @@
 # Node Class 
 class Node
   
-  attr_reader :value, :left, :right
+  attr_accessor :value, :left, :right
   def initialize(value, left = nil, right = nil)
     @value = value
     @left = left
@@ -17,21 +17,19 @@ class Tree
   end
 
   def build_tree(array)
-    array.empty? ? nil : array = array.uniq.sort
-    current_line = 1
-    levels = 1 + Math.log(array.size, 2).floor
+    array = array.uniq.sort
+    return nil if array.empty?
 
-    until current_line == levels
-      iter = Array.new()
-      for i in (0...2**(levels-1)) do
-        next
-      end
-    end
+    mid = array.length / 2
+    root = Node.new(array[mid])
+    root.left = build_tree(array[0...mid])
+    root.right = build_tree(array[mid+1..-1])
+    root
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
-    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 
